192dc6183f1d6e7401f6bd7d4053832bda4727ea
jHoon99 <eldkf1597@naver.com>
2026-03-10 10:05:00 +0900

    Ch01: flutter_layout 프로젝트 생성

---
+++ flutter_Ch01/flutter_layout/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Widget을 상하로 배치하기'),
+        ),
+        body: Body(),
+        backgroundColor: Colors.purple,
+      ),
+    ),
+  );
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.red,
+              child: Text('Container 1'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              width: 100,
+              height: 80,
+              color: Colors.green,
+              child: Text('Container 3'),
+            ),
+          ],
+        ),
+        Container(
+          width: 300,
+          height: 90,
+          color: Colors.grey,
+          child: Text('Container 4'),
+        ),
+      ],
+    );
+  }
+}
