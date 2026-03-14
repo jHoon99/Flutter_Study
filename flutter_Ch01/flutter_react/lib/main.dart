9e23d672300f6bd07188a1411336e25f695ecf86
jHoon99 <eldkf1597@naver.com>
2026-03-14 13:20:00 +0900

    Ch01: flutter_react 프로젝트 생성

---
+++ flutter_Ch01/flutter_react/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeWidget(),
+    ),
+  );
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widget을 비율로 배치하기'),
+      ),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Flexible(
+          flex: 1,
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+        Flexible(
+          flex: 2,
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+        Flexible(
+          flex: 3,
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+        Flexible(
+          flex: 4,
+          child: Container(
+            color: Colors.yellow,
+          ),
+        ),
+      ],
+    );
+  }
+}
