a453e7ba6363b73e2de4f1e8c57c498cc8c0ec34
jHoon99 <eldkf1597@naver.com>
2026-04-26 17:46:03 +0900

    Ch03: List 메서드

--- flutter_Ch02/flutter_toss_app/fast_app_base/lib/screen/main/tab/home/bank_account_dummy.dart
+++ flutter_Ch02/flutter_toss_app/fast_app_base/lib/screen/main/tab/home/bank_account_dummy.dart
@@ -55,22 +55,51 @@ class Cat extends Animal {
 }
 
 main() {
-  final dog1 = Dog();
-  final dog2 = Dog();
-  final dog3 = Dog();
-  final dog4 = Dog();
+  // final dog1 = Dog();
+  // final dog2 = Dog();
+  // final dog3 = Dog();
+  // final dog4 = Dog();
+  //
+  // final cat1 = Cat();
+  // final cat2 = Cat();
+  // final cat3 = Cat();
+  //
+  // final list = [dog1, dog2, dog3, dog4, cat1, cat2, cat3];
+  // for (final animal in list) {
+  //   // animal.eat(); 이렇게는 타입이 섞여있어서 list의 타입이 분명하지 않기때문에(Object) 불가능.
+  //   if(animal is Dog) { // 이렇게 짰다가 다른 Cat이나 Dog에 다른 객체가 들어간다면 오류를 잡기 힘들다.
+  //     animal.eat();
+  //   } else if(animal is Cat) {
+  //     animal.eat();
+  //   }
+  // }
 
-  final cat1 = Cat();
-  final cat2 = Cat();
-  final cat3 = Cat();
+  // 삽입
+  bankAccounts.insert(1, bankAccountKakao);
 
-  final list = [dog1, dog2, dog3, dog4, cat1, cat2, cat3];
-  for (final animal in list) {
-    // animal.eat(); 이렇게는 타입이 섞여있어서 list의 타입이 분명하지 않기때문에(Object) 불가능.
-    if(animal is Dog) { // 이렇게 짰다가 다른 Cat이나 Dog에 다른 객체가 들어간다면 오류를 잡기 힘들다.
-      animal.eat();
-    } else if(animal is Cat) {
-      animal.eat();
-    }
+  // 위치이동
+  final temp = bankAccounts.removeAt(4);
+  bankAccounts.insert(0, temp);
+
+  // 교환
+  // final temp1 = bankAccounts.last;
+  // bankAccounts[5] = bankAccounts[0];
+  // bankAccounts[0] = temp1;
+  bankAccounts.swap(5, 0); // 함수로 분리
+
+  // 변환
+  final _ = bankAccounts.map((e) => e.bank).toList();
+
+  for(final account in bankAccounts) {
+    print(account.toString());
+    print(account.accountTypeName);
+  }
+}
+
+extension ListExtension on List {
+  void swap(int origin, int target) {
+    final temp1 = this[target];
+    this[target] = this[origin];
+    this[origin] = temp1;
   }
 }
