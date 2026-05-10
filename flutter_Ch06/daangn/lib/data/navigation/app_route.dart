02b54f416ef76723aef06c0e0bbf69bd6565b704
jHoon99 <eldkf1597@naver.com>
2026-05-11 00:00:37 +0900

    Ch06: 라우터 구조 수정

--- flutter_Ch06/daangn/lib/data/navigation/app_route.dart
+++ flutter_Ch06/daangn/lib/data/navigation/app_route.dart
@@ -1,31 +1,27 @@
+import 'package:fast_app_base/entity/post/vo_simple_product_post.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 
 enum AppRoute {
+  // 탭
   home('/'),
   localLife('/local-life'),
   nearMe('/near_me'),
   chat('/chat'),
   my('/my'),
 
-
-
-  // home
-  notification('notification'),
-
-  // localLife
-
-  // nearMe
-
-  // chat
-
-  // My
-
+  // 전체 화면
+  notification('/notification'),
+  postDetail('/post-detail/:id'),
   ;
+
   final String path;
   const AppRoute(this.path);
 }
 
 extension AppRouteView on BuildContext {
   void pushNotification() => push('/notification');
+  void pushPostDetail(int id, SimpleProductPost post) {
+    push('/post-detail/$id', extra: post);
+  }
 }
--- flutter_Ch06/daangn/lib/data/navigation/router/router.dart
+++ flutter_Ch06/daangn/lib/data/navigation/router/router.dart
@@ -1,26 +1,55 @@
-import 'package:fast_app_base/data/navigation/router/home_branch.dart';
-import 'package:fast_app_base/data/navigation/router/local_life_branch.dart';
-import 'package:fast_app_base/data/navigation/router/near_me_branch.dart';
+import 'package:fast_app_base/data/navigation/app_route.dart';
+import 'package:fast_app_base/entity/post/vo_simple_product_post.dart';
 import 'package:fast_app_base/screen/main/s_main.dart';
+import 'package:fast_app_base/screen/main/tab/chat/f_chat.dart';
+import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
+import 'package:fast_app_base/screen/main/tab/local_life/f_local_life.dart';
+import 'package:fast_app_base/screen/main/tab/my/f_my.dart';
+import 'package:fast_app_base/screen/main/tab/near_me/f_near_me.dart';
+import 'package:fast_app_base/screen/notification/s_notification.dart';
 import 'package:go_router/go_router.dart';
 
-import 'chat_branch.dart';
-import 'my_branch.dart';
+import '../../../screen/main/tab/home/post_detail/s_post_detail.dart';
 
 final router = GoRouter(
   initialLocation: '/',
   routes: [
+    // 탭 화면들
     StatefulShellRoute.indexedStack(
       builder: (_, __, navigationShell) => MainScreen(
         navigationShell: navigationShell,
       ),
       branches: [
-        homeBranch,
-        localLifeBranch,
-        nearMeBranch,
-        chatBranch,
-        myBranch,
+        StatefulShellBranch(routes: [
+          GoRoute(path: AppRoute.home.path, builder: (_, __) => const HomeFragment()),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(path: AppRoute.localLife.path, builder: (_, __) => const LocalLifeFragment()),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(path: AppRoute.nearMe.path, builder: (_, __) => const NearMeFragmnet()),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(path: AppRoute.chat.path, builder: (_, __) => const ChatFragment()),
+        ]),
+        StatefulShellBranch(routes: [
+          GoRoute(path: AppRoute.my.path, builder: (_, __) => const MyFragment()),
+        ]),
       ],
-    )
+    ),
+
+    // 전체 화면들 (탭바 X)
+    GoRoute(
+      path: '/notification',
+      builder: (_, __) => const NotificationScreen(),
+    ),
+    GoRoute(
+      path: '/post-detail/:id',
+      builder: (_, state) {
+        final id = int.parse(state.pathParameters['id']!);
+        final post = state.extra as SimpleProductPost;
+        return PostDetailScreen(id: id, simpleProductPost: post);
+      },
+    ),
   ],
 );
